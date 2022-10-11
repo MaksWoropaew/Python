1f1ff288d03ed58235cf9daf8f987fec889d26bb
MaksWoropaew <maks.woropaew@mail.ru>
2022-10-11 18:31:38 +0300

    add tesk 2.

--- Python/HomeWork5/tesk2.py
+++ Python/HomeWork5/tesk2.py
@@ -8,3 +8,45 @@
 # b) Подумайте как наделить бота ""интеллектом""
 # Человек против человека
 
+import random
+
+cand = int(input("Введите количество конфет: "))
+max_cand = int(input("\nВведите максимальное количество конфет, которые можно забрать за один ход: "))
+person_1 = input("\nВведите имя первого игрока: ")
+person_2 = input("\nВведите имя второго игрока: ")
+draw = random.choice([person_1, person_2])
+print(f'\nПервым ходит: {draw}')
+
+def check_num(max_can, name):
+    x = int(input(f"\n{name}, можете забрать конфет в диапозоне от {1} - {max_can}: "))
+    while x < 1 or x > max_can:
+        x = int(input(f"\n{name}, можете забрать конфет в диапозоне от {1} - {max_can}: "))
+    return x
+
+def person(candy, max_candy, player_1, player_2):
+    while candy != 0:
+        if candy < max_candy:
+            max_candy = candy
+        number = check_num(max_candy, player_1)
+        candy -= number
+        print(f"\n{player_1} забрал {number} конфет, осталось {candy}")
+        if candy < max_candy:
+            max_candy = candy
+        if candy == 0:
+            print(f"\n{player_1} ПРОИГРАЛ")
+            break
+        else:
+            number = check_num(max_candy, player_2)
+            candy -= number
+            print(f"\n{player_2} забрал {number} конфет, осталось {candy}")
+            if candy == 0:
+                print(f"\n{player_2} ПРОИГРАЛ")
+                break
+            else:
+                continue
+
+if draw == person_1:
+    person(cand, max_cand, person_1, person_2)
+else:
+    person(cand, max_cand, person_2, person_1)
+  
